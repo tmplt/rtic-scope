ee8284ceb182810fa22094817078da6fdca3e047
Viktor Sonesten <v@tmplt.dev>
2021-02-10 15:57:18 +0100

    scope: port "monitor itm port 0" command

--- rtic-scope/scope/src/main.rs
+++ rtic-scope/scope/src/main.rs
@@ -1,11 +1,11 @@
-use probe_rs::{Probe, Error, MemoryInterface};
+use probe_rs::{Core, MemoryInterface, Probe};
 use std::time::Duration;
 
-fn main() -> Result<(), Error> {
+fn main() -> Result<(), probe_rs::Error> {
     // Get a list of all available debug probes
     let probes = Probe::list_all();
     if probes.is_empty() {
-        return Err(Error::UnableToOpenProbe("No probes available"));
+        return Err(probe_rs::Error::UnableToOpenProbe("No probes available"));
     }
     println!("Found {} probe(s): {:#?}", probes.len(), probes);
 
@@ -30,29 +30,50 @@ fn main() -> Result<(), Error> {
     // Check DEMCR
     const DEMCR_ADDR: u32 = 0xE000EDFC;
     let mut demcr: u32 = core.read_word_32(DEMCR_ADDR)?;
-    println!("before DEMCR: {:x}", demcr);
-    demcr |= 1 << 24;           // set TRCENA
-    core.write_word_32(DEMCR_ADDR, demcr)?;
-    let demcr: u32 = core.read_word_32(DEMCR_ADDR)?;
-    println!("readb  DEMCR: {:x}", demcr);
+    demcr |= 1 << 24; // set TRCENA
+    ensure_write_word_32(&mut core, DEMCR_ADDR, demcr)?;
 
     // Enable ITM exception tracing
     {
         // Enable exception tracing
         const DWT_CTRL_ADDR: u32 = 0xE0001000;
         let mut ctrl: u32 = core.read_word_32(DWT_CTRL_ADDR)?;
-        println!("before\tctrl = {:x}", ctrl);
-        ctrl |= 1 << 16;    // set EXCTRENA
+        ctrl |= 1 << 16; // set EXCTRENA
         ctrl &= !(1 << 12); // clear PCSAMLENA
-        println!("after\tctrl = {:x}", ctrl);
-        core.write_word_32(DWT_CTRL_ADDR, ctrl)?;
-        let ctrl: u32 = core.read_word_32(DWT_CTRL_ADDR)?;
-        println!("readb\tctrl = {:x}", ctrl);
-        // assert!(ctrl == core.read_word_32(DWT_CTRL_ADDR)?);
+        ensure_write_word_32(&mut core, DWT_CTRL_ADDR, ctrl)?;
 
-        // Enable ITM port 0
-        const ITM_TER0: u32 = 0xE0000E00;
-        core.write_word_32(ITM_TER0, 1 << 0)?;
+        // openocd: monitor itm port 0 on
+
+        // Before we do anything ITM we must first unlock the registers.
+        // TODO only do this if LAR is implemented.
+        const ITM_LAR: u32 = 0xe0000fb0;
+        const ITM_LAR_KEY: u32 = 0xc5acce55;
+        core.write_word_32(ITM_LAR, ITM_LAR_KEY)?;
+
+        // ITM_TCR
+        const ITM_TCR_ADDR: u32 = 0xE0000E80;
+        let itm_tcr = (1 << 0)  // ITMENA; master enable
+            | (1 << 3)          // TXENA; forward DWT event packets to ITM
+            | (1 << 16); // TraceBusID = 1
+        ensure_write_word_32(&mut core, ITM_TCR_ADDR, itm_tcr)?;
+
+        // ITM_TER
+        const ITM_TER0_ADDR: u32 = 0xE0000E00;
+        const ITM_TER1_ADDR: u32 = 0xE0000E04;
+        const ITM_TER7_ADDR: u32 = 0xE0000E1C;
+        ensure_write_word_32(&mut core, ITM_TER0_ADDR, 1 << 0)?; // enable port 0
+        for addr in (ITM_TER1_ADDR..=ITM_TER7_ADDR).step_by(0x4) {
+            ensure_write_word_32(&mut core, addr, 0)?; // disable all other
+        }
+    }
+
+    Ok(())
+}
+
+fn ensure_write_word_32(core: &mut Core, addr: u32, val: u32) -> Result<(), probe_rs::Error> {
+    core.write_word_32(addr, val)?;
+    if core.read_word_32(addr)? != val {
+        return Err(probe_rs::Error::UnableToOpenProbe("readback failed"));
     }
 
     Ok(())
