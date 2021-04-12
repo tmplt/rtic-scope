ba71bacd58827aab1f2d0dca28d569d3923d8d1e
Viktor Sonesten <v@tmplt.dev>
2021-04-12 16:45:54 +0200

    scopes/irq2symb: find int handlers from the ELF instead
    When the target is being traced, we don't want to halt it just to
    resolve the int handler. We look it up in the host-side ELF instead.

--- rtic-scope/scope/src/bin/irq2symb.rs
+++ rtic-scope/scope/src/bin/irq2symb.rs
@@ -1,7 +1,7 @@
 #![allow(unused_imports)]
 #![allow(dead_code)]
 
-use addr2line;
+use addr2line::{self, object::Object, object::ObjectSection};
 use anyhow::{anyhow, Context, Result};
 // use object::{Object, ObjectSection, ObjectSymbol};
 // use object;
@@ -11,6 +11,7 @@ use probe_rs::{
 };
 use probe_rs_cli_util;
 use std::cmp;
+use std::convert::TryInto;
 use std::fs::File;
 use std::path::PathBuf;
 use std::time::Duration;
@@ -33,50 +34,30 @@ fn main() -> Result<()> {
     let obj = addr2line::object::File::parse(include_bytes!(
         "/home/tmplt/exjobb/rtic-scope/target/thumbv7em-none-eabihf/debug/tracing"
     ))?;
-    let ctx = addr2line::Context::new(&obj)?;
 
-    // Get a list of all available debug probes
-    let probes = Probe::list_all();
-    if probes.is_empty() {
-        return Err(anyhow!("No probes available"));
-    }
+    // Find the interrupt handlers from the vector table
+    let int_handlers = {
+        let vt = obj
+            .section_by_name(".vector_table")
+            .context(".vector_table missing")?
+            .data()?;
 
-    // Use the first probe found
-    let probe = probes[0].open()?;
-
-    // Attach to a chip
-    let mut session = probe.attach_under_reset("stm32f4")?;
-
-    // Select a core
-    let mut core = session.core(0)?;
-
-    // Halt the attached core
-    core.halt(Duration::from_secs(5))?;
-    assert!(core.core_halted()?);
-
-    // Figure out where the vector table is
-    const VTOR_ADDR: u32 = 0xE000_ED08;
-    let vtor_tlboff = core.read_word_32(VTOR_ADDR)? >> 6;
-    let vtable = (vtor_tlboff << 6) & 0b000_0000;
-
-    // Figure out how long the vector table is
-    const ICTR_ADDR: u32 = 0xE000_E004;
-    let ictr_intlinesnum = core.read_word_32(ICTR_ADDR)? & 0b1111;
-    let int_lines = cmp::max(32 * (ictr_intlinesnum + 1), 496);
-
-    // Read entire vector table; assoc INT number with its handler address
-    // TODO find the handlers from the ELF file instead
-    let int_handlers =
-        (1..=16 + int_lines).map(|i| (i, core.read_word_32(vtable + 0x4 * i).unwrap()));
+        const WORD_LEN: usize = 32;
+        const WORD_LEN_IN_BYTES: usize = WORD_LEN / 8;
+        (1..16 + (vt.len() / WORD_LEN))
+            .zip(vt.chunks(WORD_LEN_IN_BYTES).skip(1)) // first entry is initial SP
+            .map(|(i, word)| (i, u32::from_le_bytes(word.try_into().unwrap())))
+    };
 
     // Figure out the handler function names and print them
+    let ctx = addr2line::Context::new(&obj)?;
     for (i, handler) in int_handlers {
         let function_name: String = if let Some(frame) = ctx.find_frames(handler as u64)?.next()? {
             frame.function.unwrap().demangle()?.into()
         } else {
             continue;
         };
-        println!("VTABLE[{:3}]: 0x{:016x}: {}", i, handler, function_name);
+        println!("VTABLE[{:3}]: 0x{:x}: {}", i, handler, function_name);
     }
 
     // TODO figure out the actual RTIC task name
