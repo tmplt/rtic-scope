c8c74015ca6ed9d1163e608b2600dbe5eae5f73f
Viktor Sonesten <v@tmplt.dev>
2021-04-21 16:13:28 +0200

    tracing: fully port over to RTIC v0.6

--- rtic-scope/playground/src/bin/tracing.rs
+++ rtic-scope/playground/src/bin/tracing.rs
@@ -3,19 +3,22 @@
 #![no_std]
 #![no_main]
 
-use cortex_m::asm;
+
 use panic_halt as _; // panic handler
 use rtic::app;
-use stm32f4::{self, stm32f401::Interrupt};
-use trace_examples;
 
 #[app(device = stm32f4::stm32f401, peripherals = true)]
 mod app {
-    #[init]
-    fn init(mut ctx: init::Context) -> (init::LateResources, init::Monotonics) {
-        trace_examples::enable_tracing(&mut ctx.core, &mut ctx.device);
+    use stm32f4::stm32f401::Interrupt;
+    use trace_examples;
+    use cortex_m::asm;
 
+    #[init]
+    fn init(mut ctx: init::Context)  -> (init::LateResources, init::Monotonics) {
+        trace_examples::enable_tracing(&mut ctx.core, &mut ctx.device);
         rtic::pend(Interrupt::EXTI0);
+
+        (init::LateResources {}, init::Monotonics())
     }
 
     // taben after `init` returns
