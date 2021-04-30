9ab0310afc4783f2537ba89762db955fb7036b33
Viktor Sonesten <v@tmplt.dev>
2021-04-30 03:04:29 +0200

    sw-task-trace: revamp API, use WIP tpiu/dwt cortex-m crate API

--- rtic-sw-task-trace/src/lib.rs
+++ rtic-sw-task-trace/src/lib.rs
@@ -1,29 +1,75 @@
 #![no_std]
 
-use stm32f4::stm32f401 as pac;
-
+// TODO is there an even better way to store this?
 static mut WATCH_VARIABLE: u32 = 0;
+
+pub mod setup {
+    use cortex_m::peripheral as Core;
+    use cortex_m::peripheral::{
+        dwt::{AccessType, ComparatorAddressSettings, ComparatorFunction, EmitOption},
+        tpiu::TraceProtocol,
+    };
+    use stm32f4::stm32f401 as Device;
+
+    /// Setup all core peripherals for task tracing.
+    // TODO add option to enable/disable global/local timestamps
+    pub fn core_peripherals(
+        dcb: &mut Core::DCB,
+        tpiu: &mut Core::TPIU,
+        dwt: &mut Core::DWT,
+        itm: &mut Core::ITM,
+    ) {
+        // TODO check feature availability
+
+        // enable tracing
+        dcb.enable_trace();
+
+        tpiu.set_swo_baud_rate(16_000_000, 115_200);
+        tpiu.set_trace_output_protocol(TraceProtocol::AsyncSWONRZ);
+        tpiu.enable_continuous_formatting(false); // drops ETM packets
+
+        dwt.enable_exception_tracing(true);
+        dwt.enable_pc_samples(false);
+
+        // TODO PR new functions to cortex-m
+        unsafe {
+            itm.lar.write(0xc5acce55); // unlock ITM register
+            itm.tcr.modify(|mut r| {
+                r |= 1 << 0; // ITMENA: master enable
+                r |= 1 << 1; // TSENA: enable local timestamps
+                r |= 1 << 3; // TXENA: forward DWT event packets to ITM
+                r |= 0b00 << 10; // GTSFREQ: disable global timestamps
+                r |= 1 << 16; // TraceBusID=1
+                r
+            });
+        }
+    }
+
+    pub fn device_peripherals(dbgmcu: &mut Device::DBGMCU) {
+        #[rustfmt::skip]
+        dbgmcu.cr.modify(
+            |_, w| unsafe {
+                w.trace_ioen().set_bit() // master enable for tracing
+                 .trace_mode().bits(0b00) // TRACE pin assignment for async mode (SWO)
+            },
+        );
+    }
+
+    pub fn assign_dwt_unit(mut dwt: Core::dwt::Comparator) {
+        let watch_address: u32 = unsafe { &super::WATCH_VARIABLE as *const _ } as u32;
+        // TODO do we need to clear the MATCHED, bit[24] after every match?
+        dwt.configure(ComparatorFunction::Address(ComparatorAddressSettings {
+            address: watch_address,
+            mask: 0,
+            emit: EmitOption::Data,
+            access_type: AccessType::WriteOnly,
+        }))
+        .unwrap(); // NOTE safe: valid (emit, access_type) used
+    }
+}
 
 pub fn set_current_task_id(id: u32) {
     unsafe {
         WATCH_VARIABLE = id;
-    }
-}
-
-pub fn setup_dwt(core: &mut pac::CorePeripherals) {
-    let watch_address: u32 = unsafe { &WATCH_VARIABLE as *const _ } as u32;
-    let dwt = &core.DWT.c[1];
-
-    // TODO do we need to clear the MATCHED, bit[24] after every match?
-    unsafe {
-        dwt.function.modify(|mut r| {
-            r &= !(1 << 8); // clear DATAVMATCH; perform address comparison
-            r &= !(1 << 5); // clear EMITRANGE; dont emit data trace address packets
-            r |= 0b0010; // data trace valu packet on RW match TODO change to 0b1101 (WO)
-            r
-        });
-
-        dwt.comp.write(watch_address);
-        dwt.mask.write(0);
     }
 }
