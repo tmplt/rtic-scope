53b4cba7b8a5c7c0cd3de24a290e4bc015576534
Viktor Sonesten <v@tmplt.dev>
2021-06-07 17:32:15 +0200

    rtic-apps/blinky: add some noop software tasks

--- rtic-apps/src/bin/blinky.rs
+++ rtic-apps/src/bin/blinky.rs
@@ -4,11 +4,12 @@
 use panic_halt as _; // panic handler
 use rtic::app;
 
-#[app(device = stm32f4xx_hal::stm32, peripherals = true)]
+#[app(device = stm32f4xx_hal::stm32, peripherals = true, dispatchers = [EXTI1])]
 mod app {
     use cortex_m::peripheral::syst::SystClkSource;
     use rtic_trace::{self, tracing::trace};
     use stm32f4xx_hal::stm32;
+    use cortex_m::asm;
 
     #[resources]
     struct Resources {
@@ -55,6 +56,18 @@ mod app {
         )
     }
 
+    #[task(priority = 2)]
+    #[trace]
+    fn software_task(_: software_task::Context) {
+        asm::delay(512);
+
+        #[trace]
+        fn nested() {
+            asm::delay(256);
+        }
+        nested();
+    }
+
     #[task(binds = SysTick, resources = [GPIOA])]
     fn toggle(mut ctx: toggle::Context) {
         static mut TOGGLE: bool = false;
@@ -68,5 +81,7 @@ mod app {
                 .lock(|gpioa| gpioa.bsrr.write(|w| w.br5().set_bit()));
         }
         *TOGGLE = !*TOGGLE;
+
+        software_task::spawn().unwrap();
     }
 }
