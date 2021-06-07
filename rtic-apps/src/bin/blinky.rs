1869a4d0c7a9b71d2196d38304f54196e13c9d1f
Viktor Sonesten <v@tmplt.dev>
2021-06-07 15:03:44 +0200

    rtic-apps: add blinky example

---
+++ rtic-apps/src/bin/blinky.rs
@@ -0,0 +1,55 @@
+#![no_std]
+#![no_main]
+
+use panic_halt as _; // panic handler
+use rtic::app;
+
+#[app(device = stm32f4xx_hal::stm32, peripherals = true)]
+mod app {
+    use cortex_m::peripheral::syst::SystClkSource;
+    use stm32f4xx_hal::stm32;
+
+    #[resources]
+    struct Resources {
+        GPIOA: stm32::GPIOA,
+    }
+
+    #[init]
+    fn init(ctx: init::Context) -> (init::LateResources, init::Monotonics) {
+        let mut syst = ctx.core.SYST;
+        let device = ctx.device;
+
+        // configures the system timer to trigger a SysTick exception every second
+        syst.set_clock_source(SystClkSource::Core);
+        syst.set_reload(16_000_000); // period = 1s
+        syst.enable_counter();
+        syst.enable_interrupt();
+
+        // power on GPIOA, RM0368 6.3.11
+        device.RCC.ahb1enr.modify(|_, w| w.gpioaen().set_bit());
+        // configure PA5 as output, RM0368 8.4.1
+        device.GPIOA.moder.modify(|_, w| w.moder5().bits(1));
+
+        (
+            init::LateResources {
+                GPIOA: device.GPIOA,
+            },
+            init::Monotonics(),
+        )
+    }
+
+    #[task(binds = SysTick, resources = [GPIOA])]
+    fn toggle(mut ctx: toggle::Context) {
+        static mut TOGGLE: bool = false;
+        if *TOGGLE {
+            ctx.resources
+                .GPIOA
+                .lock(|gpioa| gpioa.bsrr.write(|w| w.bs5().set_bit()));
+        } else {
+            ctx.resources
+                .GPIOA
+                .lock(|gpioa| gpioa.bsrr.write(|w| w.br5().set_bit()));
+        }
+        *TOGGLE = !*TOGGLE;
+    }
+}
